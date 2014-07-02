ae62d6bcfb6223d78567edbe7ef65f7b11e32495
Andriy Grygorenko <andriy.grygorenko@gmail.com>
2014-07-02 16:25:44 -0400

    Updated gmock script

--- build-scripts/gmock.sh
+++ build-scripts/gmock.sh
@@ -11,7 +11,8 @@ mkdir -p "$GMOCK_BUILD_LOCATION"
 
 # Cleanup gmock install sandbox
 rm -rf "$GMOCK_INSTALL_LOCATION"
-mkdir -p "$GMOCK_INSTALL_LOCATION/include"
+mkdir -p "$GMOCK_INSTALL_LOCATION/include/gmock"
+mkdir -p "$GMOCK_INSTALL_LOCATION/include/gtest"
 mkdir -p "$GMOCK_INSTALL_LOCATION/lib"
 
 # Build gmock
@@ -23,5 +24,5 @@ make -j8
 # Install gmock
 cp "libgmock.a" "../../$GMOCK_INSTALL_LOCATION/lib"
 cp "gtest/libgtest.a" "../../$GMOCK_INSTALL_LOCATION/lib"
-cp "../../$GMOCK_LOCATION/fused-src/gmock/gmock.h" "../../$GMOCK_INSTALL_LOCATION/include"
-cp "../../$GMOCK_LOCATION/fused-src/gtest/gtest.h" "../../$GMOCK_INSTALL_LOCATION/include"
+cp "../../$GMOCK_LOCATION/fused-src/gmock/gmock.h" "../../$GMOCK_INSTALL_LOCATION/include/gmock"
+cp "../../$GMOCK_LOCATION/fused-src/gtest/gtest.h" "../../$GMOCK_INSTALL_LOCATION/include/gtest"
