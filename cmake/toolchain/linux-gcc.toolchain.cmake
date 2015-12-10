c679bc67b756bd72096d3bc382edcc49b1328c4e
Andriy Grygorenko <andriy.grygorenko@gmail.com>
2015-12-10 12:54:14 -0500

    remove gets related flags

--- cmake/toolchain/linux-gcc.toolchain.cmake
+++ cmake/toolchain/linux-gcc.toolchain.cmake
@@ -10,11 +10,11 @@ set(APPLE OFF)
 set(LINUX ON)
 SET(ANDROID OFF)
 
-set( CLANG_INSTALL_ROOT "/usr" CACHE FILEPATH "Path to the root of clang install directory")
+set( GCC_INSTALL_ROOT "/usr" CACHE FILEPATH "Path to the root of clang install directory")
 set( CMAKE_SYSTEM_NAME          "Linux" CACHE STRING "Target system." )
 set( CMAKE_SYSTEM_PROCESSOR     "x86_64" CACHE STRING "Target processor." )
-set( CMAKE_C_COMPILER           "${CLANG_INSTALL_ROOT}/bin/gcc" )
-set( CMAKE_CXX_COMPILER         "${CLANG_INSTALL_ROOT}/bin/g++" )
+set( CMAKE_C_COMPILER           "${GCC_INSTALL_ROOT}/bin/gcc" )
+set( CMAKE_CXX_COMPILER         "${GCC_INSTALL_ROOT}/bin/g++" )
 
 #######################################################
 # Config
