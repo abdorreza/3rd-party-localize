69016dd0c5e82c16e7db3b01a5e3a5727fa61d30
Andriy Grygorenko <andriy.grygorenko@gmail.com>
2015-12-22 17:19:15 -0500

    switch off pangolin and scenography on mac

--- build-scripts/build-all-cmake-projects.sh
+++ build-scripts/build-all-cmake-projects.sh
@@ -82,7 +82,6 @@ else
 	echo "libceres-solver won't build for $PLATFORM_NAME"
 fi
 
-
 # libceres-solver
 if [ "$PLATFORM_NAME" = "macosx" ] || [ "$PLATFORM_NAME" = "linux-gcc" ] || [ "$PLATFORM_NAME" = "linux-gcc-4.9.2" ] || [ "$PLATFORM_NAME" = "linux-llvm" ]; then
 	/bin/sh "$REPO_ROOT/build-scripts/build-cmake-projects.sh" ceres-solver-1.11.0 ceres $PLATFORM_NAME "-DMINIGLOG=ON -DGFLAGS=OFF -DSUITESPARSE=OFF -DCXSPARSE=ON -DLAPACK=OFF -DEIGENSPARSE=ON -DDISABLE_WFORMAT=ON -DCMAKE_PREFIX_PATH=$REPO_ROOT/eigen-3.2.5;$REPO_ROOT/install/cxsparse/include/cxsparse"
@@ -95,7 +94,7 @@ else
 fi
 
 # Pangolin
-if [ "$PLATFORM_NAME" = "macosx" ] || [ "$PLATFORM_NAME" = "linux-gcc" ] || [ "$PLATFORM_NAME" = "linux-gcc-4.9.2" ] || [ "$PLATFORM_NAME" = "linux-llvm" ]; then
+if [ "$PLATFORM_NAME" = "macosx" ]; then
 	/bin/sh "$REPO_ROOT/build-scripts/build-cmake-projects.sh" Pangolin pangolin $PLATFORM_NAME "-DCPP11_NO_BOOST=ON -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_PANGOLIN_VIDEO=OFF"
 	if [ "$?" != "0" ]; then
   		echo "build-cmake-projects.sh Pangolin failed"
@@ -106,7 +105,7 @@ else
 fi
 
 # SceneGraph
-if [ "$PLATFORM_NAME" = "macosx" ] || [ "$PLATFORM_NAME" = "linux-gcc" ] || [ "$PLATFORM_NAME" = "linux-gcc-4.9.2" ] || [ "$PLATFORM_NAME" = "linux-llvm" ]; then
+if [ "$PLATFORM_NAME" = "macosx" ]; then
 	/bin/sh "$REPO_ROOT/build-scripts/build-cmake-projects.sh" SceneGraph SceneGraph $PLATFORM_NAME "-DEXPORT_SceneGraph=OFF -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_PREFIX_PATH=$REPO_ROOT/eigen-3.2.5;$REPO_ROOT/install/pangolin;$REPO_ROOT/install/pangolin/platform-include/macosx;$REPO_ROOT/install/pangolin/lib/macosx"
 
 	if [ "$?" != "0" ]; then
@@ -116,4 +115,3 @@ if [ "$PLATFORM_NAME" = "macosx" ] || [ "$PLATFORM_NAME" = "linux-gcc" ] || [ "$
 else
 	echo "SceneGraph won't build for $PLATFORM_NAME"
 fi
-
